60c5e221f923d4a4591fd0da5c9b10a8e653f667
Luka Markušić <markusicluka@gmail.com>
2024-01-10 23:11:14 +0100

    update the finder to work with new data format

--- finder.sh
+++ finder.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 movie_data=$(python3 main.py)
-while true; do
 
-    # Select a movie
-    selected_movie=$(echo "$movie_data" | jq -r 'keys_unsorted[]' | fzf)
+# Select a movie
+selected_movie=$(echo "$movie_data" | jq -r 'keys_unsorted[]' | fzf)
 
-    # Check if user pressed "q" to quit
-    if [ -z "$selected_movie" ]; then
-        break
-    fi
+# Check if user pressed "q" to quit
+if [ -z "$selected_movie" ]; then
+    break
+fi
 
-    # Display selected movie details
-    selected_movie_data=$(echo "$movie_data" | jq -r --arg movie_name "$selected_movie" '.[$movie_name]')
-    echo "$selected_movie_data" | jq --arg selected_movie "$selected_movie" -r '.[0][0] as $cinema_name | .[0][1] | sort_by(.datetime)[] | "\($selected_movie) - \( $cinema_name) - \(.datetime) - \(.attributes | sort | join(" - "))"' | fzf
-done
+# Display selected movie details
+selected_movie_data=$(echo "$movie_data" | jq -r --arg movie_name "$selected_movie" '.[$movie_name]')
+echo "$selected_movie_data"
+# kaboom
